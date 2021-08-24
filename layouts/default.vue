eba0e0251d558043b8b8e3465753423bd1cb3ba6
HoshinoSuzumi <boxlab@foxmail.com>
2021-08-25 05:12:26 +0800

    简单抽屉

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,17 @@
   <div class="wrapper">
     <div class="background"></div>
     <div class="mask blur"></div>
+    <div class="drawer" :class="{'open': drawerActive}">Drawer</div>
     <div class="main">
       <div class="navbar">
+        <button class="menu-toggle" @click="drawerActive=!drawerActive">
+          <i class="uit uit-subject"></i>
+        </button>
         <div class="title">
           ALLTALE
           <span class="subtitle">University</span>
         </div>
-        <div class="actions">
+        <div class="actions hide-on-mobile">
           <nuxt-link class="btn" to="/none">示例</nuxt-link>
           <nuxt-link class="btn" to="/none">示例</nuxt-link>
           <nuxt-link class="btn" to="/none">示例</nuxt-link>
@@ -24,7 +28,29 @@
 <script>
 export default {
   data() {
-    return {}
-  },
+    return {
+      drawerActive: false,
+    }
+  }
 }
 </script>
+
+<style scoped>
+@import "https://unicons.iconscout.com/release/v4.0.0/css/thinline.css";
+
+.drawer {
+  z-index: 999;
+  position: fixed;
+  left: -300px;
+  bottom: 0;
+  width: 300px;
+  height: calc(100% - 64px);
+  background: rgba(0, 0, 0, .8);
+  backdrop-filter: blur(50px) saturate(180%);
+  transition: left var(--animation) ease-out;
+}
+
+.drawer.open {
+  left: 0;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,14 +12,15 @@ export default {
       {name: 'format-detection', content: 'telephone=no'}
     ],
     link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
-    ]
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {rel: 'stylesheet', href: 'https://unicons.iconscout.com/release/v4.0.0/css/thinline.css'}
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/css/alltale-web.css',
-    '@/assets/css/buefy.css'
+    '@/assets/css/buefy.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
