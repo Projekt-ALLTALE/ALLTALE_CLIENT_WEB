c2c5c027673845bea9962a1e6bfd7293d3947f82
HoshinoSuzumi <boxlab@foxmail.com>
2021-09-09 20:31:47 +0800

    pwa test

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,9 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+  buildModules: [
+    '@nuxtjs/pwa'
+  ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
